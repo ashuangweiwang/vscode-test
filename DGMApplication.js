bf365fe0f23ee395f9cf59ed8cf22b97b4a15c52
ashuangweiwang <ashuangweiwang@163.com>
2022-01-19 17:38:06 +0800

    网格动态绘制提交

--- DGMApplication.js
+++ DGMApplication.js
@@ -7,9 +7,9 @@ class DGMApplication {
         this.WorkSpace;
 
         this.RendererApp = new PIXI.Application({
-            backgroundColor: 0x1099bb,
-            width: 800,         // default: 800 宽度
-            height: 600,        // default: 600 高度
+            backgroundColor: 0xFFFFFF,
+            //width: 1000,         // default: 800 宽度
+            //height: 800,        // default: 600 高度
         });
 
         curDocument.body.appendChild(this.RendererApp.view);
@@ -19,6 +19,7 @@ class DGMApplication {
 
         this.WorkSpaceStage = new PIXI.Container; //绘图
         this.frameStage = new FrameStage();//背景
+        
 
         this.RendererApp.stage.addChild(this.WorkSpaceStage);
         this.RendererApp.stage.addChild(this.frameStage);
@@ -35,45 +36,7 @@ class DGMApplication {
     }
 
     CreateFrameStage() {
-        this.WorkSpace = new DGMWorkSpace();
-        var Map = new DGMGeoMap2D();
-        Map.Name = 'map';
-        this.WorkSpace.AddObject(Map);
-
-        var Layer1 = new DGMGraphicsLayer();
-        Layer1.Name = 'lay';
-        Map.AddObject(Layer1);
-
-        var Tile = new DGMTile();
-        Layer1.AddObject(Tile);
-
-        var graphicsFeature = new GraphicsFeature();
-
-        Tile.FeatureCollection.AddObject(graphicsFeature);
-
-
-
-        graphicsFeature.id = 1;
-        graphicsFeature.Name = 'gra';
-
-
-        var data = new GraphicData();
-        data.GeoType = 'Line';
-        data.points.push(100);
-        data.points.push(100);
-        data.points.push(500);
-        data.points.push(600);
- 
-        graphicsFeature.AddGraphicData(data);
-
-        this.zoomScale[0] = this.RendererApp.view.width / 0.05;
-        this.zoomScale[1] = this.RendererApp.view.height / 0.1;
-
-        this.RendererApp.stage.addChild(graphicsFeature);
-        this.RendererApp.stage.x = this.RendererApp.stage.x - 0.10;
-        this.RendererApp.stage.y = this.RendererApp.stage.y - 0.50; //12400
-
-        graphicsFeature.DrawGraphic();
+        
 
 
 
--- FrameStage.js
+++ FrameStage.js
@@ -16,5 +16,50 @@ class FrameStage extends PIXI.Container {
         this.CellSize = size;
 
     }
-    DrawLine() {}
+    DrawLine() {
+        //取得画布（根容器Stage）的长宽
+        var appwidth=app.renderer.view.width;
+        var appheight=app.renderer.view.height;
+        var delta=40;//网格间隙
+
+        var line_h = new Array();
+        for (var i = 0; i < (appwidth-40)/delta-1; i++) {
+            line_h[i] = new PIXI.Graphics;
+            line_h[i].lineStyle(1, 0x00FF00, 1);
+            line_h[i].moveTo(0, 0);
+            line_h[i].lineTo(0, appheight-40);
+            line_h[i].x = 20 + (i + 1) * delta;
+            line_h[i].y = 20;
+            app.stage.addChild(line_h[i]);
+        }
+
+        var line_a = new Array();
+        for (var i = 0; i < (appheight-40)/delta-1; i++) {
+            line_a[i] = new PIXI.Graphics;
+            line_a[i].lineStyle(1, 0x00FF00, 1);
+            line_a[i].moveTo(0, 0);
+            line_a[i].lineTo(appwidth-40, 0);
+            line_a[i].x = 20;
+            line_a[i].y = 20 + (i + 1) * delta;;
+            app.stage.addChild(line_a[i]);
+        }
+        var linex = new PIXI.Graphics;
+        linex.lineStyle(1, 0x000000, 1);
+        linex.moveTo(0, 0);
+        linex.lineTo(appwidth-40, 0);
+        linex.x = 20;
+        linex.y = appheight-20;
+        app.stage.addChild(linex);
+
+        var liney = new PIXI.Graphics;
+        liney.lineStyle(1, 0x000000, 1);
+        liney.moveTo(0, 0);
+        liney.lineTo(0, appheight-40);
+        liney.x = 20;
+        liney.y = 20;
+        app.stage.addChild(liney);
+
+
+
+    }
 }
